10c164ace12b65bb4ffc39eb58d7de6e3ce7ef2a
Dario Astorga <dastorga@ignetworks.com>
2019-12-20 17:48:44 -0300

    add IsOwner class

--- api/permissions.py
+++ api/permissions.py
@@ -1,5 +1,11 @@
+# permissions.py
 from rest_framework import permissions
 
-class IsOwner(permissions.BasePermission):
-    message = "No es propietario"
 
+class IsOwner(permissions.BasePermission):
+    message = "No es el propietario."
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == obj.owner
